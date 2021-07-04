4deeb9d5eb9bedac7609b6a283be20235227dcbe
FressMaschine35 <84859919+FressMaschine35@users.noreply.github.com>
2021-07-04 16:36:52 +0200

    Test
    Alle Änderungen von Vorher wieder verworfen und somit wieder auf dem Ausgangspunkt von Steven

--- PortfolioGUI/src/application/Main.java
+++ PortfolioGUI/src/application/Main.java
@@ -1,18 +1,24 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	
 	@SuppressWarnings("exports")
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
 		try {
 					
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("FXMLMain.fxml"));
