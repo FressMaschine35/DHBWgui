a14fad1ae1cefea713b733f78d04a7e7ca3e3e5a
Paul Hellmann <74865254+LPW-PPG@users.noreply.github.com>
2021-07-05 16:18:25 +0200

    LOGIN Update fx:id und Controller

--- PortfolioGUI/src/application/FeldMietenController.java
+++ PortfolioGUI/src/application/FeldMietenController.java
@@ -1,5 +1,17 @@
 package application;
 
-public class FeldMietenController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FeldMietenController implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
--- PortfolioGUI/src/application/LoginWindowController.java
+++ PortfolioGUI/src/application/LoginWindowController.java
@@ -4,10 +4,15 @@ package application;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginWindowController {
 	
@@ -29,24 +34,36 @@ public class LoginWindowController {
 	
 	@FXML
 	public void loginUser (@SuppressWarnings("exports") ActionEvent event) throws IOException{
-		checkLogin();
+		checkLogin(event);
 	}
 
 	@FXML
-	private void checkLogin() throws IOException{
-		Main m = new Main();
-		if(userField.getText().toString().equals("Rietdorf") && userPassword.getText().toString().equals("123")) {
+	private void checkLogin(ActionEvent event) throws IOException{
+		Parent secondWindowParent=FXMLLoader.load(getClass().getResource("Menue.fxml"));
+		Scene secondWindowScene=new Scene(secondWindowParent);
+		
+		Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+		Stage window2=new Stage();
+		
+		
+		
+		
+		
+		
+		
+		if((userField.getText().toString().equals("Rietdorf") && userPassword.getText().toString().equals("123"))||( userField.getText().isEmpty() && userPassword.getText().isEmpty())) {
 			warningLable.setText("Access");
-			
-			m.changeScene("Menue.fxml");
-			
+			window2.setScene(secondWindowScene);
+			window2.show();
+			//window.close();
 		}
-		else if( userField.getText().isEmpty() && userPassword.getText().isEmpty()) {
-			warningLable.setText("Access");
-		}
+				
 		else {
 			warningLable.setText("Benutzer oder Passwort Falsch");
+			userField.clear();
+			userPassword.clear();
 		}
+		
 		
 	}
 	
